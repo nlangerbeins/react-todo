9a0cac31d7b1a2ed41ee24293c192e357c534897
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2023-12-12 22:31:14 +0100

    Fetch Data from Airtable

--- src/App.js
+++ src/App.js
@@ -6,24 +6,57 @@ const App = () => {
   const [todoList, setTodoList] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
-  const fetchData = () =>
-    new Promise((resolve, reject) =>
-      setTimeout(
-        () =>
-          resolve({
-            data: {
-              todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [],
-            },
-          }),
-        2000
-      )
-    );
+  const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
+
+  const fetchData = async () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        const message = `Error: ${response.status}`;
+        throw new Error(message);
+      }
+
+      const data = await response.json();
+
+      const todos = data.records.map((todo) => {
+        const newTodo = {
+          id: todo.id,
+          title: todo.fields.title,
+        };
+        return newTodo;
+      });
+
+      setTodoList(todos);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   React.useEffect(() => {
-    fetchData().then((result) => {
-      setTodoList(result.data.todoList);
-      setIsLoading(false);
-    });
+    // new Promise((resolve, reject) =>
+    //   setTimeout(
+    //     () =>
+    //       resolve({
+    //         data: {
+    //           todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [],
+    //         },
+    //       }),
+    //     2000
+    //   )
+    // ).then((result) => {
+    //   setTodoList(result.data.todoList);
+    //   setIsLoading(false);
+    // });
+    fetchData();
   }, []);
 
   React.useEffect(() => {
